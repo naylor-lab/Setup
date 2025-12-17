96d6b92d24a82aa605c40df68d7ff4677f51e72c
Open Source <naylor-laboratory@tutamail.com>
2025-12-17 09:25:59 -0300

    Criar default.sh

---
+++ src/install/default.sh
@@ -0,0 +1,13 @@
+pkg update -y
+pkg upgrade -y
+
+#Dependencies:
+pkg install termux-x11-nightly -y
+pkg install pulseaudio -y
+
+pkg install tur-repo -y
+pkg install firefox -y
+
+pkg install x11-repo
+pkg install wget -y
+pkg install xfce4 -y
