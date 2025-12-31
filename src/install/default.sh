58f28706ecb45e692474e73fd38c412940e6b515
Open Source <209934679+naylor-lab@users.noreply.github.com>
2025-12-31 08:21:51 -0300

    Clean up package installation commands in default.sh
    Removed unnecessary package installations from the script.

--- src/install/default.sh
+++ src/install/default.sh
@@ -2,16 +2,4 @@ pkg update -y
 pkg upgrade -y
 
 #Dependencies:
-pkg install root-repo
-pkg install x11-repo
 
-pkg install termux-x11-nightly
-
-
-pkg install pulseaudio -y
-
-pkg install tur-repo -y
-pkg install firefox
-
-pkg install wget -y
-pkg install xfce4 -y
