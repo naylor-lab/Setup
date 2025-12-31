4b7c7258bbdf3edb661d6d832a47ef8ac388c310
Open Source <209934679+naylor-lab@users.noreply.github.com>
2025-12-31 08:20:00 -0300

    Delete src/install/desktop.sh

--- src/install/desktop.sh
+++
@@ -1,7 +0,0 @@
-cd ~
-
-wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh
-
-chmod +x ~/startxfce4_termux.sh
-
-bash ~/startxfce4_termux.sh
